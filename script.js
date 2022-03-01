972bc0be71312ce726b52909520865e9401053e1
Strunk <strunk@MacBook-Pro.local>
2022-02-28 23:06:26 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log(21 + 21);
+// numbers, strings, booleans, null, undefined
+// + - * / ** %
+console.log(5 % 2); console.log(5 % 4);
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+console.log("Hawk" + "eye");
+console.log("Amy Strunk".length);
+console.log("Avengers:Endgame"[15])
+
+// bonus 1
+console.log("\\");
+// bonus 2
+console.log(5 ** 4 - 9); //616
+// bonus 3
+console.log("I".length - "Am".length + "Iron".length - "Man".length);
+// bonus 4
+console.log("CAP"[1]); // A
+console.log("VISION"[0]); // V
+console.log("OKOYE"[4]); // E
+console.log("NICK FURY"[0]); // N
+console.log("GROOT"[0]); // G
+console.log("DOCTOR STRANGE"[13]); // E
+console.log("THOR"[3]); // R
+console.log("STAR-LORD"[0]); // S
+// bonus 5
+var m=25;
+var c=4;
+var u = m + c;
+console.log(u)
